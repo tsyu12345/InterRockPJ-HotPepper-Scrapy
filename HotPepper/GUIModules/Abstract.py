e96437381d33d7f7fc845a044dd157087069ebbf
syu_asus_Note <syukunt@icloud.com>
2022-03-16 18:53:21 +0900

    developブランチ作成。ベースpythonファイルの追加。このブランチを開発のベースブランチとする。基本的に機能開発を行う場合、このブランチを親に「feature/~~」で新規ブランチを切ること。

---
+++ HotPepper/GUIModules/Abstract.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+from typing import Any, Iterator, Optional, Final as const, callable
+from abc import ABCMeta, abstractmethod
+from PySimpleGUI.PySimpleGUI import Window
+
+class AbsGUIComponent(object, metaclass=ABCMeta):
+    
+    """_summary_\n
+    GUIコンポーネントの基底抽象クラス定義
+    """
+    
+    @abstractmethod
+    def __lay_out(self) -> list[list[Any]]:
+        """_summary_\n
+        GUIコンポーネントのレイアウト配列を返す。
+        """
+        pass
+    
+class AbsWindowComponent(object, metaclass=ABCMeta):
+    """[summary]\n
+    windowレベルの基底抽象クラス定義。
+    """
+    
+    def __init__(self, window_name=None)-> None:
+        self.event_handler:dict[str, list[callable , tuple]] = {}
+        self.event:str = ""
+        self.value:dict[str, str] = {}
+        self.window:Window = Window(window_name)
+    
+    @abstractmethod
+    def _lay_out(self) -> list[list[any]]:
+        """_summary_\n
+        レイアウト配列を返す\n
+        Returns:\n
+            list[list]: GUIコンポーネントを格納した2次元配列\n
+        """
+        pass
+    
+    def addEventListener(self, key:str, callback:callable, *args) -> None:
+        """_summary_\n
+        イベントリスナーを設定する。\n
+        Args:\n
+            key: イベントキー\n
+            callback: イベントハンドラー\n
+            args: イベントハンドラーに渡す引数\n
+        """
+        self.event_handler[key] = [callback, args]
+        print(self.event_handler)
+        
+    def __catchEvent(self) -> None:
+        """_summary_\n
+        イベントをキャッチする。\n
+        """
+        if self.event in self.event_handler:
+            
+            callback:callable = self.event_handler[self.event][0]
+            args:tuple = self.event_handler[self.event][1]
+            
+            if args == ():
+                callback()
+            else:
+                callback(args)
+    
+    def display(self) -> None:
+        """_summary_\n
+        ウィンドウを表示する.
+        must use in while loop.
+        """
+        self.event, self.value = self.window.read()
+        self.__catchEvent()
+    
+    @abstractmethod
+    def dispose(self) -> None:
+        """_summary_\n
+        ウィンドウを破棄する
+        """
+        pass
